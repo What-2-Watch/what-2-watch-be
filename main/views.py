86d53e9ffc7e1d7bcc45599b9ada6f71976d5b3b
Hope Gochnour <hope.gochnour@gmail.com>
2021-05-17 10:47:21 -0600

    Add serializer for user functions
    Add users urls with v1 versioning
    
    add new pips to requirements

--- main/views.py
+++ main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from main.models import CustomUser
+from main.serializers import UserSerializer
+from rest_framework.decorators import api_view, action
+from rest_framework import viewsets
+from main.models import CustomUser
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    # @api_view(['GET', 'PUT', 'DELETE'])
+    # def user_detail(request, pk):
+        # """
+        # Retrieve, update or delete a user.
+        # """
+        # try:
+        #     user = CustomUser.objects.get(pk=pk)
+        # except CustomUser.DoesNotExist:
+        #     return Response(status=status.HTTP_404_NOT_FOUND)
+        #
+        # if request.method == 'GET':
+        #     serializer = UserSerializer(user)
+        #     return Response(serializer.data)
+        #
+        # elif request.method == 'PUT':
+        #     serializer = UserSerializer(user, data=request.data)
+        #     if serializer.is_valid():
+        #         serializer.save()
+        #         return Response(serializer.data)
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #
+        # elif request.method == 'DELETE':
+        #     user.delete()
+        #     return Response(status=status.HTTP_204_NO_CONTENT)

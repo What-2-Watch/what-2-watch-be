e5ee922086b38865ee5a8be9612526268da8b428
Trevor Robinson <trevor.robi1254@gmail.com>
2021-05-18 21:26:18 -0600

    add user subscriptions table

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,6 +5,7 @@
 # def test_function():
 #     assert f() == 3
 from main.models import CustomUser
+from main.models import Subscription
 from django.test import TestCase
 
 def test_user():
@@ -15,3 +16,9 @@ def test_user():
     assert str(user.first_name) == 'john'
     assert str(user.last_name) == 'doe'
     assert str(user.region) == 'NA'
+
+def test_subscription():
+    user = CustomUser(password="123456",first_name="john", last_name="doe", email="johndoe@blah.com", language='1234', region='NA')
+
+    sub = user.subscription_set.create(name="netflux", api_provider_id=8)
+    breakpoint()

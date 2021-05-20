516c494178997349093a5d067decc87201a75ae2
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2021-05-20 14:27:04 -0600

    Remove commented code from tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,8 @@
-# def f():
-#     return 3
-
-
-# def test_function():
-#     assert f() == 3
 import pytest
 from main.models import CustomUser
 from main.models import Subscription
 from django.test import TestCase
-# @pytest.mark.django_db
-# @pytest.fixture(autouse=True)
-# def enable_db_access_for_all_tests(db):
-#     pass
+
 
 def test_user():
     user = CustomUser(password="123456",first_name="john", last_name="doe", email="johndoe@blah.com", language='1234', region='NA')
@@ -36,11 +27,3 @@ class SubscriptionTestCase(TestCase):
         record = Subscription.objects.get(id=1)
         # breakpoint()
         self.assertEqual(record.user.first_name, "john")
-
-# user.subscription_set.all().first()
-# this is how you access user subscriptions might want to look into reverse relationship?
-# https://stackoverflow.com/questions/17328910/django-what-is-reverse-relationship
-    # assert user.subscriptions.count() == 1
-
-    # assert user.subscriptions.count 1
-    # breakpoint()
